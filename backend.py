fcc2133ffa9a4446f46d32d0a691de646343d4b9
Zamer <aelizamer95@gmail.com>
2024-03-10 22:32:43 +0500

    Added images to sky conditions

--- backend.py
+++ backend.py
@@ -3,19 +3,15 @@ import requests
 api_key = "3e3bbd8a6c780941f0435a929e4bfedf"
 
 
-def get_data(place, forecast_days=None, type_data=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     result = requests.get(url)
     data = result.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if type_data == "Temperature":
-        filtered_data = [i["main"]["temp"] for i in filtered_data]
-    if type_data == "Sky":
-        filtered_data = [i["weather"][0]["main"] for i in filtered_data]
     return filtered_data
 
 
-if __name__=="__main__":
-    print(get_data("Faisalabad", 4, "Temperature"))
+if __name__ == "__main__":
+    print(get_data("Faisalabad", 4))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as sl
 import plotly.express as px
 from backend import get_data
 
-
+# Added title, text input, slider, selectbox and subheader
 sl.title("Weather Forecast for the Next Days")
 place = sl.text_input("Place:")
 days = sl.slider("Forecast Days", max_value=5, min_value=1,
@@ -10,7 +10,21 @@ days = sl.slider("Forecast Days", max_value=5, min_value=1,
 type_data = sl.selectbox("Select data to view", ("Temperature", "Sky"))
 sl.subheader(f"{type_data} for the next {days} days in {place}")
 
-d , t = get_data(place, days, type_data)
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
-sl.plotly_chart(figure)
+    if type_data == "Temperature":
+        temperature = [i["main"]["temp"] for i in filtered_data]
+        date = [i["dt_txt"] for i in filtered_data]
+        # Create a temperature plot
+        figure = px.line(x=date, y=temperature, labels={'x': "Date", 'y': "Temperature"})
+        sl.plotly_chart(figure)
+
+    if type_data == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_conditions = [i["weather"][0]["main"] for i in filtered_data]
+        image_paths = [images[conditions] for conditions in sky_conditions]
+        # Render icons for sky
+        sl.image(image_paths, width=115)
