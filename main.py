70aea7d4e8d35b9288fc7883bba9d2ba7ceead4e
Zamer <aelizamer95@gmail.com>
2024-03-10 22:56:17 +0500

    Fixed Some Bugs

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ place = sl.text_input("Place:")
 days = sl.slider("Forecast Days", max_value=5, min_value=1,
                  help="Select the number of forecasted days")
 type_data = sl.selectbox("Select data to view", ("Temperature", "Sky"))
-sl.subheader(f"{type_data} for the next {days} days in {place}")
 
 if place:
-    # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
+        sl.subheader(f"{type_data} for the next {days} days in {place}")
 
-    if type_data == "Temperature":
-        temperature = [i["main"]["temp"] for i in filtered_data]
-        date = [i["dt_txt"] for i in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=date, y=temperature, labels={'x': "Date", 'y': "Temperature"})
-        sl.plotly_chart(figure)
+        if type_data == "Temperature":
+            temperature = [i["main"]["temp"]/10 for i in filtered_data]
+            date = [i["dt_txt"] for i in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=date, y=temperature, labels={'x': "Date", 'y': "Temperature"})
+            sl.plotly_chart(figure)
 
-    if type_data == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [i["weather"][0]["main"] for i in filtered_data]
-        image_paths = [images[conditions] for conditions in sky_conditions]
-        # Render icons for sky
-        sl.image(image_paths, width=115)
+        if type_data == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [i["weather"][0]["main"] for i in filtered_data]
+            image_paths = [images[conditions] for conditions in sky_conditions]
+            # Render icons for sky
+            sl.image(image_paths, width=115)
+    except KeyError:
+        sl.error("Please Enter A Correct City Name!!!!!".upper())
